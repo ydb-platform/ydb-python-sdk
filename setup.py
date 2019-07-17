a3ee4c559e6ef0179d9057c5d8630384a4f0e683
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2019-07-17 13:49:29 +0300

    sync upstream ref:099759e85340decc6af2bb5ed3b5f7dea6df7338

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='ydb',
-    version='0.0.27',
+    version='0.0.28',
     description='YDB Python library',
     author='Yandex LLC',
     author_email='ydb@yandex-team.ru',
