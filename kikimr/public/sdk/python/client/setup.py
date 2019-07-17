7c8834084707e38ff1803609718ba5d9ca18a22c
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2019-07-17 13:46:00 +0300

    sync upstream ref:1ebde500e91c63aaf8524090b3c101b126dde594

--- kikimr/public/sdk/python/client/setup.py
+++ kikimr/public/sdk/python/client/setup.py
@@ -93,7 +93,7 @@ class BuildPackageProtos(setuptools.Command):
 
 setuptools.setup(
     name='ydb',
-    version='0.0.24',
+    version='0.0.25',
     description='Yandex DB Python library',
     author='ydb@',
     author_email='ydb@yandex-team.ru',
