d5c3a297557350a6efe9374bfe15dcfb579d2ef7
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2019-07-17 13:46:43 +0300

    sync upstream ref:9959361818ed80f3a686e2e820c8d96ad75224af

--- kikimr/public/sdk/python/client/setup.py
+++ kikimr/public/sdk/python/client/setup.py
@@ -58,24 +58,6 @@ class CopyModules(setuptools.Command):
                     )
 
 
-def classifiers_list():
-    return [
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-    ]
-
-
-def requirements_list():
-    return (
-        'protobuf>=3.3.0',
-        'grpcio>=1.5.0',
-        'enum-compat>=0.0.1'
-    )
-
-
 class BuildPackageProtos(setuptools.Command):
     description = 'build svngrpc protobuf modules'
     user_options = []
@@ -94,15 +76,25 @@ class BuildPackageProtos(setuptools.Command):
 setuptools.setup(
     name='ydb',
     version='0.0.25',
-    description='Yandex DB Python library',
-    author='ydb@',
+    description='YDB Python library',
+    author='Yandex LLC',
     author_email='ydb@yandex-team.ru',
-    url='https://ydb.yandex-team.ru/docs/',
-    license='Yandex NDA',
+    url='http://github.com/yandex-cloud/ydb-python-sdk',
+    license='Apache 2.0',
     package_dir={'': '.'},
     packages=setuptools.find_packages('.'),
-    classifiers=classifiers_list(),
-    install_requires=requirements_list(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=(
+        'protobuf>=3.3.0',
+        'grpcio>=1.5.0',
+        'enum-compat>=0.0.1'
+    ),
     cmdclass={
         'preprocess': CopyModules,
         'build_package_protos': BuildPackageProtos,
