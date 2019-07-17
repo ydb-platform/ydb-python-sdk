4858bb3b45b2c7b55aacb0e937c4d38a983bd05a
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2019-07-17 13:46:48 +0300

    sync upstream ref:b2056780f22347a7bd820699de59a093b306fa8c

--- setup.py
+++ setup.py
@@ -1,76 +1,5 @@
 # -*- coding: utf-8 -*-
-import os
-import shutil
 import setuptools
-
-
-def get_arcadia_root():
-    arcadia_root_candidate = os.path.dirname(os.path.abspath(__file__))
-    retries_count = 10
-    for _ in range(retries_count):
-        if os.path.exists(os.path.join(arcadia_root_candidate, '.arcadia.root')):
-            return arcadia_root_candidate
-        arcadia_root_candidate = os.path.dirname(arcadia_root_candidate)
-    raise RuntimeError("Unable to find .arcadia.root!")
-
-
-class CopyModules(setuptools.Command):
-    description = 'copy proto files into the root directory'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        arcadia_root = get_arcadia_root()
-
-        for relpath in [
-            'yql/public/types',
-            'yql/public/issue/protos',
-            'kikimr/public/api/grpc',
-            'kikimr/public/api/protos',
-            'kikimr/public/sdk/python/client',
-            'kikimr/public/sdk/python/tvm',
-            'kikimr/public/sdk/python/iam',
-        ]:
-            parts = relpath.split('/')
-
-            for p_size in range(1, len(parts) + 1):
-                f_path = os.path.join(*tuple(parts[:p_size]))
-                if not os.path.exists(f_path):
-                    os.mkdir(f_path)
-
-                if not os.path.exists(os.path.join(f_path, '__init__.py')):
-                    open(os.path.join(f_path, '__init__.py'), 'a').close()
-
-            source_folder = os.path.join(arcadia_root, relpath)
-            for file_name in os.listdir(source_folder):
-                if file_name.startswith('setup.py'):
-                    continue
-
-                if file_name.endswith('.proto') or file_name.endswith('.py'):
-                    shutil.copyfile(
-                        os.path.join(source_folder, file_name),
-                        os.path.join(relpath, file_name)
-                    )
-
-
-class BuildPackageProtos(setuptools.Command):
-    description = 'build svngrpc protobuf modules'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        from grpc_tools import command
-        command.build_package_protos(self.distribution.package_dir[''])
 
 
 setuptools.setup(
@@ -93,10 +22,6 @@ setuptools.setup(
     install_requires=(
         'protobuf>=3.3.0',
         'grpcio>=1.5.0',
-        'enum-compat>=0.0.1'
-    ),
-    cmdclass={
-        'preprocess': CopyModules,
-        'build_package_protos': BuildPackageProtos,
-    }
+        'enum-compat>=0.0.1',
+    )
 )
