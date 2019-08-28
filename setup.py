f40455f8215785e5dcc79e401f51f46a317aa4ad
gvit <gvit@yandex-team.ru>
2019-08-28 14:29:20 +0300

    alter version
    Note: mandatory check (NEED_CHECK) was skipped
    ref:5550304
    sync: https://proxy.sandbox.yandex-team.ru/1094464826

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='ydb',
-    version='0.0.35',
+    version='0.0.36',
     description='YDB Python library',
     author='Yandex LLC',
     author_email='ydb@yandex-team.ru',
