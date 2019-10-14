fc3a1acb34a0ea223646ae24b2bacd1e12fa4c83
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2019-10-14 15:14:37 +0300

    sync upstream ref:fd9409bc54b8cd4425ef1cde4215bcf63f17d1e5

--- kikimr/public/api/grpc/draft/ydb_experimental_v1_pb2.py
+++ kikimr/public/api/grpc/draft/ydb_experimental_v1_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='kikimr/public/api/grpc/draft/ydb_experimental_v1.proto',
   package='Ydb.Experimental.V1',
   syntax='proto3',
-  serialized_pb=_b('\n6kikimr/public/api/grpc/draft/ydb_experimental_v1.proto\x12\x13Ydb.Experimental.V1\x1a/kikimr/public/api/protos/ydb_experimental.proto2\xab\x03\n\x13\x45xperimentalService\x12W\n\nUploadRows\x12#.Ydb.Experimental.UploadRowsRequest\x1a$.Ydb.Experimental.UploadRowsResponse\x12Z\n\x0bReadColumns\x12$.Ydb.Experimental.ReadColumnsRequest\x1a%.Ydb.Experimental.ReadColumnsResponse\x12l\n\x11GetShardLocations\x12*.Ydb.Experimental.GetShardLocationsRequest\x1a+.Ydb.Experimental.GetShardLocationsResponse\x12q\n\x12\x45xecuteStreamQuery\x12+.Ydb.Experimental.ExecuteStreamQueryRequest\x1a,.Ydb.Experimental.ExecuteStreamQueryResponse0\x01\x42 \n\x1e\x63om.yandex.ydb.experimental.v1b\x06proto3')
+  serialized_pb=_b('\n6kikimr/public/api/grpc/draft/ydb_experimental_v1.proto\x12\x13Ydb.Experimental.V1\x1a/kikimr/public/api/protos/ydb_experimental.proto2\xe1\x01\n\x13\x45xperimentalService\x12W\n\nUploadRows\x12#.Ydb.Experimental.UploadRowsRequest\x1a$.Ydb.Experimental.UploadRowsResponse\x12q\n\x12\x45xecuteStreamQuery\x12+.Ydb.Experimental.ExecuteStreamQueryRequest\x1a,.Ydb.Experimental.ExecuteStreamQueryResponse0\x01\x42 \n\x1e\x63om.yandex.ydb.experimental.v1b\x06proto3')
   ,
   dependencies=[kikimr_dot_public_dot_api_dot_protos_dot_ydb__experimental__pb2.DESCRIPTOR,])
 
@@ -39,7 +39,7 @@ _EXPERIMENTALSERVICE = _descriptor.ServiceDescriptor(
   index=0,
   options=None,
   serialized_start=129,
-  serialized_end=556,
+  serialized_end=354,
   methods=[
   _descriptor.MethodDescriptor(
     name='UploadRows',
@@ -51,27 +51,9 @@ _EXPERIMENTALSERVICE = _descriptor.ServiceDescriptor(
     options=None,
   ),
   _descriptor.MethodDescriptor(
-    name='ReadColumns',
-    full_name='Ydb.Experimental.V1.ExperimentalService.ReadColumns',
-    index=1,
-    containing_service=None,
-    input_type=kikimr_dot_public_dot_api_dot_protos_dot_ydb__experimental__pb2._READCOLUMNSREQUEST,
-    output_type=kikimr_dot_public_dot_api_dot_protos_dot_ydb__experimental__pb2._READCOLUMNSRESPONSE,
-    options=None,
-  ),
-  _descriptor.MethodDescriptor(
-    name='GetShardLocations',
-    full_name='Ydb.Experimental.V1.ExperimentalService.GetShardLocations',
-    index=2,
-    containing_service=None,
-    input_type=kikimr_dot_public_dot_api_dot_protos_dot_ydb__experimental__pb2._GETSHARDLOCATIONSREQUEST,
-    output_type=kikimr_dot_public_dot_api_dot_protos_dot_ydb__experimental__pb2._GETSHARDLOCATIONSRESPONSE,
-    options=None,
-  ),
-  _descriptor.MethodDescriptor(
     name='ExecuteStreamQuery',
     full_name='Ydb.Experimental.V1.ExperimentalService.ExecuteStreamQuery',
-    index=3,
+    index=1,
     containing_service=None,
     input_type=kikimr_dot_public_dot_api_dot_protos_dot_ydb__experimental__pb2._EXECUTESTREAMQUERYREQUEST,
     output_type=kikimr_dot_public_dot_api_dot_protos_dot_ydb__experimental__pb2._EXECUTESTREAMQUERYRESPONSE,
