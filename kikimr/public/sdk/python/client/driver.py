e431f53789fdbc5c91a9124bb9eb641b0e0783e5
pnv1 <pnv1@yandex-team.ru>
2019-10-30 15:17:16 +0300

    KIKIMR-7764 Keep alive in python SDK
    ref:5878548
    sync: https://proxy.sandbox.yandex-team.ru/1189831913

--- kikimr/public/sdk/python/client/driver.py
+++ kikimr/public/sdk/python/client/driver.py
@@ -4,12 +4,13 @@ from . import credentials as credentials_impl, table, scheme, pool
 
 class DriverConfig(object):
     __slots__ = ('endpoint', 'database', 'ca_cert', 'channel_options', 'credentials', 'use_all_nodes',
-                 'root_certificates', 'certificate_chain', 'private_key')
+                 'root_certificates', 'certificate_chain', 'private_key', 'grpc_keep_alive_timeout')
 
     def __init__(
             self, endpoint, database=None, ca_cert=None, auth_token=None,
             channel_options=None, credentials=None, use_all_nodes=False,
-            root_certificates=None, certificate_chain=None, private_key=None):
+            root_certificates=None, certificate_chain=None, private_key=None,
+            grpc_keep_alive_timeout=None):
         """
         A driver config to initialize a driver instance
         :param endpoint: A endpoint specified in pattern host:port to be used for initial
@@ -24,6 +25,7 @@ class DriverConfig(object):
         private key should be used.
         :param certificate_chain: The PEM-encoded certificate chain as a byte string
         to use or or None if no certificate chain should be used.
+        :param grpc_keep_alive_timeout: GRpc KeepAlive timeout, ms
         """
         self.endpoint = endpoint
         self.database = database
@@ -36,9 +38,14 @@ class DriverConfig(object):
         self.root_certificates = root_certificates
         self.certificate_chain = certificate_chain
         self.private_key = private_key
+        self.grpc_keep_alive_timeout = grpc_keep_alive_timeout
 
     def set_database(self, database):
         self.database = database
+        return self
+
+    def set_grpc_keep_alive_timeout(self, timeout):
+        self.grpc_keep_alive_timeout = timeout
         return self
 
 
