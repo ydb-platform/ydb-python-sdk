5aad8e4bc4dac27751a67829e37d520fa6ecfe55
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2020-02-21 14:33:24 +0300

    sync upstream ref:70d707a227ce2b4f916777136ccf9eca54577a8f

--- kikimr/public/sdk/python/client/_utilities.py
+++ kikimr/public/sdk/python/client/_utilities.py
@@ -24,7 +24,7 @@ def future():
 
 
 def x_ydb_sdk_build_info_header():
-    return ("x-ydb-sdk-build-info", "python_sdk_revision: 0.0.37.1")
+    return ("x-ydb-sdk-build-info", "python_sdk_revision: 0.0.39")
 
 
 # Decorator that ensures no exceptions are leaked from decorated async call
