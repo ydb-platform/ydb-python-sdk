1d7737967da09e13644f7da3687bb231a3cf6648
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2020-05-14 15:07:07 +0300

    sync upstream ref:a314b184abe4d3a9490f9fe63ace53fc332a4d20

--- kikimr/public/sdk/python/client/experimental.py
+++ kikimr/public/sdk/python/client/experimental.py
@@ -34,6 +34,14 @@ class ExperimentalClient(object):
     def __init__(self, driver):
         self.driver = driver
 
+    def async_execute_stream_query(self, yql_text, parameters=None, settings=None):
+        """
+        That is experimental api. Can be used only for tests, not for production.
+        """
+        request = _execute_stream_query_request_factory(yql_text, parameters, settings)
+        stream_it = self.driver(request, ydb_experimental_v1_pb2_grpc.ExperimentalServiceStub, 'ExecuteStreamQuery', settings=settings)
+        return _utilities.AsyncResponseIterator(stream_it, _wrap_stream_query_response)
+
     def execute_stream_query(self, yql_text, parameters=None, settings=None):
         """
         That is experimental api. Can be used only for tests, not for production.
