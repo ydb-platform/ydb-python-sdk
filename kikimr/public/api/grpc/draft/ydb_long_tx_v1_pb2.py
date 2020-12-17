d6fc3ded08654e3ca8951d6dd0b339bcfba17247
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2020-12-17 19:13:46 +0300

    sync upstream ref:6efa8d0b679cdea9a6a6a5acf71e7737013b4898

--- kikimr/public/api/grpc/draft/ydb_long_tx_v1_pb2.py
+++ kikimr/public/api/grpc/draft/ydb_long_tx_v1_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='kikimr/public/api/grpc/draft/ydb_long_tx_v1.proto',
   package='Ydb.LongTx.V1',
   syntax='proto3',
-  serialized_pb=_b('\n1kikimr/public/api/grpc/draft/ydb_long_tx_v1.proto\x12\rYdb.LongTx.V1\x1a\x30kikimr/public/api/protos/draft/ydb_long_tx.proto2\x93\x02\n\rLongTxService\x12\x42\n\x07\x42\x65ginTx\x12\x1a.Ydb.LongTx.BeginTxRequest\x1a\x1b.Ydb.LongTx.BeginTxResponse\x12\x45\n\x08\x43ommitTx\x12\x1b.Ydb.LongTx.CommitTxRequest\x1a\x1c.Ydb.LongTx.CommitTxResponse\x12<\n\x05Write\x12\x18.Ydb.LongTx.WriteRequest\x1a\x19.Ydb.LongTx.WriteResponse\x12\x39\n\x04Read\x12\x17.Ydb.LongTx.ReadRequest\x1a\x18.Ydb.LongTx.ReadResponseB\x1b\n\x19\x63om.yandex.ydb.long_tx.v1b\x06proto3')
+  serialized_pb=_b('\n1kikimr/public/api/grpc/draft/ydb_long_tx_v1.proto\x12\rYdb.LongTx.V1\x1a\x30kikimr/public/api/protos/draft/ydb_long_tx.proto2\x96\x03\n\rLongTxService\x12T\n\x07\x42\x65ginTx\x12#.Ydb.LongTx.BeginTransactionRequest\x1a$.Ydb.LongTx.BeginTransactionResponse\x12W\n\x08\x43ommitTx\x12$.Ydb.LongTx.CommitTransactionRequest\x1a%.Ydb.LongTx.CommitTransactionResponse\x12]\n\nRollbackTx\x12&.Ydb.LongTx.RollbackTransactionRequest\x1a\'.Ydb.LongTx.RollbackTransactionResponse\x12<\n\x05Write\x12\x18.Ydb.LongTx.WriteRequest\x1a\x19.Ydb.LongTx.WriteResponse\x12\x39\n\x04Read\x12\x17.Ydb.LongTx.ReadRequest\x1a\x18.Ydb.LongTx.ReadResponseB\x1b\n\x19\x63om.yandex.ydb.long_tx.v1b\x06proto3')
   ,
   dependencies=[kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2.DESCRIPTOR,])
 
@@ -39,15 +39,15 @@ _LONGTXSERVICE = _descriptor.ServiceDescriptor(
   index=0,
   options=None,
   serialized_start=119,
-  serialized_end=394,
+  serialized_end=525,
   methods=[
   _descriptor.MethodDescriptor(
     name='BeginTx',
     full_name='Ydb.LongTx.V1.LongTxService.BeginTx',
     index=0,
     containing_service=None,
-    input_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._BEGINTXREQUEST,
-    output_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._BEGINTXRESPONSE,
+    input_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._BEGINTRANSACTIONREQUEST,
+    output_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._BEGINTRANSACTIONRESPONSE,
     options=None,
   ),
   _descriptor.MethodDescriptor(
@@ -55,14 +55,23 @@ _LONGTXSERVICE = _descriptor.ServiceDescriptor(
     full_name='Ydb.LongTx.V1.LongTxService.CommitTx',
     index=1,
     containing_service=None,
-    input_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._COMMITTXREQUEST,
-    output_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._COMMITTXRESPONSE,
+    input_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._COMMITTRANSACTIONREQUEST,
+    output_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._COMMITTRANSACTIONRESPONSE,
+    options=None,
+  ),
+  _descriptor.MethodDescriptor(
+    name='RollbackTx',
+    full_name='Ydb.LongTx.V1.LongTxService.RollbackTx',
+    index=2,
+    containing_service=None,
+    input_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._ROLLBACKTRANSACTIONREQUEST,
+    output_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._ROLLBACKTRANSACTIONRESPONSE,
     options=None,
   ),
   _descriptor.MethodDescriptor(
     name='Write',
     full_name='Ydb.LongTx.V1.LongTxService.Write',
-    index=2,
+    index=3,
     containing_service=None,
     input_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._WRITEREQUEST,
     output_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._WRITERESPONSE,
@@ -71,7 +80,7 @@ _LONGTXSERVICE = _descriptor.ServiceDescriptor(
   _descriptor.MethodDescriptor(
     name='Read',
     full_name='Ydb.LongTx.V1.LongTxService.Read',
-    index=3,
+    index=4,
     containing_service=None,
     input_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._READREQUEST,
     output_type=kikimr_dot_public_dot_api_dot_protos_dot_draft_dot_ydb__long__tx__pb2._READRESPONSE,
