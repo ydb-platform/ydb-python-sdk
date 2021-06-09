a5e397109a1f1f24cf5a83f4674f5347e92108dc
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2021-06-09 20:58:21 +0300

    sync upstream ref:0654a09b3f8b3001e56ab90f76007c60f38f985b

---
+++ ydb/sqlalchemy/example_py3/__main__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, unicode_literals
+import sys
+
+
+mode = sys.argv[1] if len(sys.argv) > 2 else 'functional'
+if mode == 'functional':
+    from . import functional
+    functional.run()
+else:
+    raise RuntimeError('Unknown mode: ' + mode)
