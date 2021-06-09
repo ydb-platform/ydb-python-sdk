0654a09b3f8b3001e56ab90f76007c60f38f985b
gvit <gvit@yandex-team.ru>
2021-06-09 20:52:48 +0300

    move sqlalchemy and to add ydb package
    ref:8254129
    sync: https://proxy.sandbox.yandex-team.ru/2215280564

---
+++ ydb/sqlalchemy/__init__.py
@@ -0,0 +1,192 @@
+"""
+Experimental
+Work in progress, breaking changes are possible.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import ydb
+from ydb.dbapi.errors import NotSupportedError
+
+
+try:
+    from sqlalchemy.engine.default import DefaultDialect
+    from sqlalchemy.sql.compiler import IdentifierPreparer, GenericTypeCompiler, SQLCompiler
+    from sqlalchemy import Table
+    import sqlalchemy as sa
+
+    class YqlIdentifierPreparer(IdentifierPreparer):
+        def __init__(self, dialect):
+            super(YqlIdentifierPreparer, self).__init__(
+                dialect,
+                initial_quote='`',
+                final_quote='`',
+            )
+
+    class YqlTypeCompiler(GenericTypeCompiler):
+        def visit_VARCHAR(self, type_, **kw):
+            return "STRING"
+
+        def visit_unicode(self, type_, **kw):
+            return "UTF8"
+
+        def visit_NVARCHAR(self, type_, **kw):
+            return "UTF8"
+
+        def visit_TEXT(self, type_, **kw):
+            return "UTF8"
+
+        def visit_FLOAT(self, type_, **kw):
+            return "DOUBLE"
+
+        def visit_BOOLEAN(self, type_, **kw):
+            return "BOOL"
+
+    class YqlCompiler(SQLCompiler):
+        def visit_function(self, func, add_to_result_map=None, **kwargs):
+            # Copypaste of `sa.sql.compiler.SQLCompiler.visit_function` with
+            # `::` as namespace separator instead of `.`
+            if add_to_result_map is not None:
+                add_to_result_map(func.name, func.name, (), func.type)
+
+            disp = getattr(self, "visit_%s_func" % func.name.lower(), None)
+            if disp:
+                return disp(func, **kwargs)
+            else:
+                name = sa.sql.compiler.FUNCTIONS.get(func.__class__, None)
+                if name:
+                    if func._has_args:
+                        name += "%(expr)s"
+                else:
+                    name = func.name
+                    name = (
+                        self.preparer.quote(name)
+                        if self.preparer._requires_quotes_illegal_chars(name)
+                        or isinstance(name, sa.sql.elements.quoted_name)
+                        else name
+                    )
+                    name = name + "%(expr)s"
+                return "::".join(
+                    [
+                        (
+                            self.preparer.quote(tok)
+                            if self.preparer._requires_quotes_illegal_chars(tok)
+                            or isinstance(name, sa.sql.elements.quoted_name)
+                            else tok
+                        )
+                        for tok in func.packagenames
+                    ]
+                    + [name]
+                ) % {"expr": self.function_argspec(func, **kwargs)}
+
+    COLUMN_TYPES = {
+        ydb.PrimitiveType.Int8: sa.INTEGER,
+        ydb.PrimitiveType.Int16: sa.INTEGER,
+        ydb.PrimitiveType.Int32: sa.INTEGER,
+        ydb.PrimitiveType.Int64: sa.INTEGER,
+        ydb.PrimitiveType.Uint8: sa.INTEGER,
+        ydb.PrimitiveType.Uint16: sa.INTEGER,
+        ydb.PrimitiveType.Uint32: sa.INTEGER,
+        ydb.PrimitiveType.Uint64: sa.INTEGER,
+        ydb.PrimitiveType.Float: sa.FLOAT,
+        ydb.PrimitiveType.Double: sa.FLOAT,
+        ydb.PrimitiveType.String: sa.TEXT,
+        ydb.PrimitiveType.Utf8: sa.TEXT,
+        ydb.PrimitiveType.Json: sa.JSON,
+        ydb.DecimalType: sa.DECIMAL,
+        ydb.PrimitiveType.Yson: sa.TEXT,
+        ydb.PrimitiveType.Date: sa.DATE,
+        ydb.PrimitiveType.Datetime: sa.DATETIME,
+        ydb.PrimitiveType.Timestamp: sa.DATETIME,
+        ydb.PrimitiveType.Interval: sa.INTEGER,
+    }
+
+    def _get_column_type(t):
+        if isinstance(t.item, ydb.DecimalType):
+            return sa.DECIMAL(precision=t.item.precision, scale=t.item.scale)
+
+        return COLUMN_TYPES[t.item]
+
+    class YqlDialect(DefaultDialect):
+        name = 'yql'
+        supports_alter = False
+        max_identifier_length = 63
+        supports_sane_rowcount = False
+
+        supports_native_enum = False
+        supports_native_boolean = True
+        supports_smallserial = False
+
+        supports_sequences = False
+        sequences_optional = True
+        preexecute_autoincrement_sequences = True
+        postfetch_lastrowid = False
+
+        supports_default_values = False
+        supports_empty_insert = False
+        supports_multivalues_insert = True
+        default_paramstyle = 'qmark'
+
+        isolation_level = None
+
+        preparer = YqlIdentifierPreparer
+        statement_compiler = YqlCompiler
+        type_compiler = YqlTypeCompiler
+
+        def __init(self, **kwargs):
+            super(DefaultDialect, self).__init__(**kwargs)
+
+        @staticmethod
+        def dbapi():
+            import ydb.dbapi
+            return ydb.dbapi
+
+        def get_columns(self, connection, table_name, schema=None, **kw):
+            if schema is not None:
+                raise NotSupportedError
+
+            if isinstance(table_name, Table):
+                qt = table_name.name
+            else:
+                qt = table_name
+
+            columns = connection.raw_connection().describe(qt)
+            as_compatible = []
+            for column in columns:
+                as_compatible.append(
+                    {
+                        'name': column.name, 'type': _get_column_type(column.type),
+                        'nullable': True,
+                    }
+                )
+
+            return as_compatible
+
+        def has_table(self, connection, table_name, schema=None):
+            if schema is not None:
+                raise NotSupportedError
+
+            quote = self.identifier_preparer.quote_identifier
+            qtable = quote(table_name)
+
+            # TODO: use `get_columns` instead.
+            statement = 'SELECT * FROM ' + qtable
+            try:
+                connection.execute(statement)
+                return True
+            except Exception:
+                return False
+
+except ImportError:
+    class YqlDialect(object):
+        def __init__(self):
+            raise RuntimeError('could not import sqlalchemy')
+
+
+def register_dialect(
+        name='yql',
+        module=__name__,
+        cls='YqlDialect',
+):
+    import sqlalchemy as sa
+    return sa.dialects.registry.register(name, module, cls)
