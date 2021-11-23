dd0f82b635b78f85ab55dca0ae7f24524fc2aa5c
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2021-11-23 11:21:05 +0300

    sync upstream ref:f612f9d6924e40db517ae6fa68c51b0cbc8333f9

--- kikimr/public/sdk/python/tests/conftest.py
+++ kikimr/public/sdk/python/tests/conftest.py
@@ -1,6 +1,8 @@
 import os
 import pytest
 import ydb
+import time
+import shutil
 
 
 @pytest.fixture(scope="module")
@@ -8,15 +10,60 @@ def docker_compose_file(pytestconfig):
     return os.path.join(str(pytestconfig.rootdir), "docker-compose.yml")
 
 
+def wait_container_ready(driver):
+    driver.wait(timeout=10)
+
+    with ydb.SessionPool(driver) as pool:
+
+        started_at = time.time()
+        while time.time() - started_at < 30:
+            try:
+                with pool.checkout() as session:
+                    session.execute_scheme(
+                        "create table `.sys_health/test_table` (A int32, primary key(A));"
+                    )
+
+                return True
+
+            except ydb.Error as e:
+                time.sleep(1)
+
+    raise RuntimeError("Container is not ready after timeout.")
+
+
 @pytest.fixture(scope="module")
-def endpoint(module_scoped_container_getter):
-    service = module_scoped_container_getter.get("ydb").network_info[0]
-    return service.hostname + ":" + service.host_port
+def endpoint(pytestconfig, module_scoped_container_getter):
+    with ydb.Driver(endpoint="localhost:2136", database="/local") as driver:
+        wait_container_ready(driver)
+    yield "localhost:2136"
+
+    shutil.rmtree(os.path.join(str(pytestconfig.rootdir), "ydb_certs"))
+
+
+@pytest.fixture(scope="module")
+def secure_endpoint(pytestconfig, module_scoped_container_getter):
+    ca_path = os.path.join(str(pytestconfig.rootdir), "ydb_certs/ca.pem")
+    iterations = 0
+    while not os.path.exists(ca_path) and iterations < 10:
+        time.sleep(1)
+        iterations += 1
+
+    assert os.path.exists(ca_path)
+    os.environ["YDB_SSL_ROOT_CERTIFICATES_FILE"] = ca_path
+    with ydb.Driver(
+        endpoint="grpcs://localhost:2135",
+        database="/local",
+        root_certificates=ydb.load_ydb_root_certificate(),
+    ) as driver:
+        wait_container_ready(driver)
+    yield "localhost:2135"
+
+    shutil.rmtree(os.path.join(str(pytestconfig.rootdir), "ydb_certs"))
 
 
 @pytest.fixture(scope="module")
 def database():
-    return "local"
+    return "/local"
 
 
 @pytest.fixture()
