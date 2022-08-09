840f3cfd6faafd33f95958fc8e49505b9f6a790b
Vitalii Gridnev <gridnevvvit@gmail.com>
2022-08-09 23:22:59 +0300

    fixes in ydb.aio library

---
+++ tests/aio/test_tx.py
@@ -0,0 +1,38 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_tx_commit(driver, database):
+    session = await driver.table_client.session().create()
+    prepared = await session.prepare(
+        "DECLARE $param as Int32;\n SELECT $param as value",
+    )
+
+    tx = session.transaction()
+    await tx.execute(prepared, {"$param": 2})
+    await tx.commit()
+    await tx.commit()
+
+
+@pytest.mark.asyncio
+async def test_tx_rollback(driver, database):
+    session = await driver.table_client.session().create()
+    prepared = await session.prepare(
+        "DECLARE $param as Int32;\n SELECT $param as value",
+    )
+
+    tx = session.transaction()
+    await tx.execute(prepared, {"$param": 2})
+    await tx.rollback()
+    await tx.rollback()
+
+
+@pytest.mark.asyncio
+async def test_tx_begin(driver, database):
+    session = await driver.table_client.session().create()
+    await session.create()
+
+    tx = session.transaction()
+    await tx.begin()
+    await tx.begin()
+    await tx.rollback()
