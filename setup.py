7a2ece5f8dd4098c502c73958f7daa3b8cbdc5d2
robot <robot@umbrella>
2023-01-19 06:50:55 +0000

    Release:

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as r:
 
 setuptools.setup(
     name="ydb",
-    version="3.0.0a0",  # AUTOVERSION
+    version="3.0.1b1",  # AUTOVERSION
     description="YDB Python SDK",
     author="Yandex LLC",
     author_email="ydb@yandex-team.ru",
