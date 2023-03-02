9e0e44d8d59114f91a36c12e7617bad747fea034
Timofey Koolin <timofey.koolin@gmail.com>
2023-03-02 17:35:01 +0300

    fix typos

--- tests/topics/test_topic_reader.py
+++ tests/topics/test_topic_reader.py
@@ -15,11 +15,11 @@ class TestTopicReaderAsyncIO:
         self, driver, topic_path, topic_with_messages, topic_consumer
     ):
 
-        reader = driver.topic_client.topic_reader(topic_consumer, topic_path)
+        reader = driver.topic_client.reader(topic_consumer, topic_path)
         batch = await reader.receive_batch()
         await reader.commit_with_ack(batch)
 
-        reader = driver.topic_client.topic_reader(topic_consumer, topic_path)
+        reader = driver.topic_client.reader(topic_consumer, topic_path)
         batch2 = await reader.receive_batch()
         assert batch.messages[0] != batch2.messages[0]
 
@@ -36,10 +36,10 @@ class TestTopicReaderSync:
     def test_read_and_commit_message(
         self, driver_sync, topic_path, topic_with_messages, topic_consumer
     ):
-        reader = driver_sync.topic_client.topic_reader(topic_consumer, topic_path)
+        reader = driver_sync.topic_client.reader(topic_consumer, topic_path)
         batch = reader.receive_batch()
         reader.commit_with_ack(batch)
 
-        reader = driver_sync.topic_client.topic_reader(topic_consumer, topic_path)
+        reader = driver_sync.topic_client.reader(topic_consumer, topic_path)
         batch2 = reader.receive_batch()
         assert batch.messages[0] != batch2.messages[0]
