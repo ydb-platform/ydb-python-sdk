4ede48a783b83feb01924ccd2bf232fed5f043cb
Timofey Koolin <timofey.koolin@gmail.com>
2023-03-20 11:46:15 +0300

    remove warning for good choise

--- ydb/global_settings.py
+++ ydb/global_settings.py
@@ -7,10 +7,6 @@ from . import table
 def global_allow_truncated_result(enabled: bool = True):
     if enabled:
         warnings.warn("Global allow truncated response is deprecated behaviour.")
-    else:
-        warnings.warn(
-            "Global deny truncated response is default behaviour. You don't need call the function."
-        )
 
     convert._default_allow_truncated_result = enabled
 
@@ -18,9 +14,5 @@ def global_allow_truncated_result(enabled: bool = True):
 def global_allow_split_transactions(enabled: bool):
     if enabled:
         warnings.warn("Global allow truncated response is deprecated behaviour.")
-    else:
-        warnings.warn(
-            "Global deby truncated response is default behaviour. You don't need call the function."
-        )
 
     table._allow_split_transaction = enabled
