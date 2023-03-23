e3b8118d09fe6fe2b5950d6e74c861a443b9078d
Timofey Koolin <timofey.koolin@gmail.com>
2023-03-23 03:45:54 +0300

    group messages to batches

---
+++ ydb/_topic_writer/topic_writer_test.py
@@ -0,0 +1,52 @@
+from typing import List
+
+import pytest
+
+from .topic_writer import _split_messages_by_size
+
+
+@pytest.mark.parametrize(
+    "messages,split_size,expected",
+    [
+        (
+            [1, 2, 3],
+            0,
+            [[1], [2], [3]],
+        ),
+        (
+            [1, 2, 3],
+            1,
+            [[1], [2], [3]],
+        ),
+        (
+            [1, 2, 3],
+            3,
+            [[1, 2], [3]],
+        ),
+        (
+            [1, 2, 3],
+            100,
+            [[1, 2, 3]],
+        ),
+        (
+            [100, 2, 3],
+            100,
+            [[100], [2, 3]],
+        ),
+        (
+            [],
+            100,
+            [],
+        ),
+        (
+            [],
+            100,
+            [],
+        ),
+    ],
+)
+def test_split_messages_by_size(
+    messages: List[int], split_size: int, expected: List[List[int]]
+):
+    res = _split_messages_by_size(messages, split_size, lambda x: x)  # noqa
+    assert res == expected
