ecb2cd7cf9966df2a9c14c0848ca19292d04509c
Oleg Ovcharuk <vgvoleg@gmail.com>
2024-09-10 13:35:41 +0300

    add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,7 @@ html_theme_options = {
 }
 
 html_logo = '_static/logo.svg'
+html_favicon = '_static/logo.svg'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
