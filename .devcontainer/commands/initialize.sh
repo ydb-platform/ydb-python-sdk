98d2d96622247c0753f1eae5034846b05d16d91d
Vladislav Polyakov <polrk@ydb.tech>
2025-04-22 14:50:13 +0300

    Add sign-off configuration and GPG settings in initialization scripts
    Signed-off-by: Vladislav Polyakov <polrk@ydb.tech>

--- .devcontainer/commands/initialize.sh
+++ .devcontainer/commands/initialize.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-git config --local user.email "$(git config user.email)"
+git config --local format.signoff true
 git config --local user.name "$(git config user.name)"
+git config --local user.email "$(git config user.email)"
