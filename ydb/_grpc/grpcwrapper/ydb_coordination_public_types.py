a5a36f6e1c75ec4ed5e287a7510096321686bfef
slampy <slampy97@gmail.com>
2025-10-29 15:34:55 +0100

    fix config

--- ydb/_grpc/grpcwrapper/ydb_coordination_public_types.py
+++ ydb/_grpc/grpcwrapper/ydb_coordination_public_types.py
@@ -7,31 +7,23 @@ if typing.TYPE_CHECKING:
 else:
     from ..common.protos import ydb_coordination_pb2
 
-class ConsistencyMode(Enum):
-    STRICT = 0
-    RELAXED = 1
-
-class RateLimiterCountersMode(Enum):
-    NONE = 0
-    BASIC = 1
-    FULL = 2
 
 @dataclass
 class NodeConfig:
-    attach_consistency_mode: ConsistencyMode
+    attach_consistency_mode: ydb_coordination_pb2.ConsistencyMode
     path: str
-    rate_limiter_counters_mode: RateLimiterCountersMode
-    read_consistency_mode: ConsistencyMode
+    rate_limiter_counters_mode: ydb_coordination_pb2.RateLimiterCountersMode
+    read_consistency_mode: ydb_coordination_pb2.ConsistencyMode
     self_check_period_millis: int
     session_grace_period_millis: int
 
     @staticmethod
     def from_proto(msg: ydb_coordination_pb2.Config) -> "NodeConfig":
         return NodeConfig(
-            attach_consistency_mode=ConsistencyMode(msg.attach_consistency_mode),
+            attach_consistency_mode=ydb_coordination_pb2.ConsistencyMode(msg.attach_consistency_mode),
             path=msg.path,
-            rate_limiter_counters_mode=RateLimiterCountersMode(msg.rate_limiter_counters_mode),
-            read_consistency_mode=ConsistencyMode(msg.read_consistency_mode),
+            rate_limiter_counters_mode=ydb_coordination_pb2.RateLimiterCountersMode(msg.rate_limiter_counters_mode),
+            read_consistency_mode=ydb_coordination_pb2.ConsistencyMode(msg.read_consistency_mode),
             self_check_period_millis=msg.self_check_period_millis,
             session_grace_period_millis=msg.session_grace_period_millis,
         )
