2f8d64e1d6b7a93cb766e0d2d1d7b9f1936c834e
Nikolay Makhalin <makhalinnick@gmail.com>
2026-03-16 18:27:38 +0100

    Update credentials.py

--- ydb/aio/credentials.py
+++ ydb/aio/credentials.py
@@ -62,7 +62,7 @@ class AbstractExpiringTokenCredentials(credentials.AbstractExpiringTokenCredenti
 
         except Exception as e:
             self.last_error = str(e)
-            self.logger.error("Failed to refresh token async: %s", e, exc_info=e)
+            self.logger.error("Failed to refresh token async: %s", e, exc_info=True)
             if should_raise:
                 raise issues.ConnectionError(
                     "%s: %s.\n%s" % (self.__class__.__name__, self.last_error, self.extra_error_message)
