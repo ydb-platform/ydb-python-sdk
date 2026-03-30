e182284f089dfefaa2c42e874a067a6f37072f8a
Idris Yandarov <32651311+r142f@users.noreply.github.com>
2026-03-30 22:14:52 +0300

    fix: commit to rerun checks

--- ydb/aio/_utilities.py
+++ ydb/aio/_utilities.py
@@ -110,6 +110,7 @@ async def _check_fastest_endpoint(
         for t in tasks:
             if not t.done():
                 t.cancel()
+
         await asyncio.gather(*tasks, return_exceptions=True)
 
 
