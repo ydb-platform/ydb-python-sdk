084be43e734a4fe7cc362f009ea4310bf275d9d7
Oleg Ovcharuk <vgvoleg@gmail.com>
2026-05-28 17:54:42 +0300

    Update test_errors.py

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -43,4 +43,4 @@ def test_scheme_error(driver_sync, database):
     assert type(exc.value) is ydb.issues.SchemeError
     assert exc.value.status == server_code
     assert f"server_code: {server_code}" in str(exc.value)
-    assert "Path does not exist" in str(exc.value)
+    assert "does not exist" in str(exc.value)
